6dad2cf7b4ad27f24941418444d11a8d580222b4
YingQiZhong <840545482@qq.com>
2020-11-17 16:41:23 +0800

    The character(travel) page and image can be showed

--- app/Controllers/Characters.php
+++ app/Controllers/Characters.php
@@ -5,29 +5,44 @@ namespace App\Controllers;
 class Characters extends BaseController {
 
     public function index() {
+        $places = new \App\Models\People(); 
+        $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $displayHeadings = array_slice($headings, 1, 2);
+        $table->setHeading(array_map('ucfirst', $displayHeadings));
+        $records = $places->findAll();
+        foreach ($records as $record) {
+            $imageString = '<img src="/image/'.$record->image.'"/>';
+            $table->addRow($record->id, $record->name, $record->country, $record->brith, $record->setting, $record->description, $record->disposition, $record->link, $imageString);
+        }
+        return $table->generate(); 
+
+
+
         // connect to the model 
-        $places = new \App\Models\People(); 
+        $places = new \App\Models\People();
         // retrieve all the records    
-        $records = $places->findAll(); 
-    
-     // get a template parser     
-     $parser = \Config\Services::parser();    
-     // tell it about the substitions
-     return $parser->setData(['records' => $records])       
-            // and have it render the template with those       
-            ->render('peoplelist'); 
+        
+        // get a template parser     
+        $parser = \Config\Services::parser();
+        // tell it about the substitions
+        return $parser->setData(['records' => $records])
+                        // and have it render the template with those       
+                        ->render('peoplelist');
     }
-    public function showme($id)     {     
+
+    public function showme($id) {
         // connect to the model     
-        $places = new \App\Models\People(); 
+        $places = new \App\Models\People();
         // retrieve all the records    
         $record = $places->find($id);
-        
+
         // get a template parser   
-        $parser = \Config\Services::parser(); 
+        $parser = \Config\Services::parser();
         // tell it about the substitions    
-        return $parser->setData($record) 
-                // and have it render the template with those
-                ->render('onepeople'); 
-    } 
+        return $parser->setData($record)
+                        // and have it render the template with those
+                        ->render('onepeople');
+    }
+
 }
